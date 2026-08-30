982b34cbe72a0922a8c8452a33fbd5e91f915a5e
Esdras Domingos <esdrascaxitodeveloper@yahoo.com>
2026-08-30 07:04:45 +0100

    Estruturas de repetição - Parte 1
    Estrutura de repetição com teste lógico no inicio (while)
    Estrutura de repetição com teste lógico no final (do while)

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+let counter = 1;
+
+do{
+    console.log(`Tudo bem - Passo ${counter}`);
+    
+    counter++;
+
+}while(counter <= 6)
+
+// while(counter <= 500){
+//     console.log(`Tudo bem - Passo ${counter}`);
+    
+//     counter++;
+// }
+
+// console.log("Tudo bem ?");
+// console.log("Tudo bem ?");
+// console.log("Tudo bem ?");
+// console.log("Tudo bem ?");
+// console.log("Tudo bem ?");
+// console.log("Tudo bem ?");
