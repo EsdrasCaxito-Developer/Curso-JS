6bc04fc4175c7882abab5a3ef410ccee769b92c2
Esdras Domingos <esdrascaxitodeveloper@yahoo.com>
2026-09-03 04:53:25 +0100

    Exercícios Javascript - Parte 6
    Tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,24 @@
-alert("Olá")
+const gerarTabuada = () =>{
+    let numero = document.getElementById('numero'),
+        tabuada = document.getElementById('seltab');
+
+    let n = Number(numero.value);
+
+    if(n.length == 0){
+        alert("Por favor, digite um número")
+    }   
+    else{
+        let c = 1;
+        tabuada.innerHTML = `<option value="">
+                               Digite um número acima
+                             </option>`;
+
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = \t${n*c}`;
+            item.value = `${c}`;
+            tabuada.appendChild(item);
+            c++;
+        }
+    }
+}
